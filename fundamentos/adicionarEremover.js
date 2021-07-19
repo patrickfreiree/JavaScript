aba5a137b7b68f97b684b0fbf5c8ac0161ae9997
Patrick <66400161+patrickfreiree@users.noreply.github.com>
2021-07-19 09:49:11 -0300

    Create adicionarEremover.js

---
+++ fundamentos/adicionarEremover.js
@@ -0,0 +1,10 @@
+const notas = [10,6,8]
+
+notas.push(7)  // push = coloca na array 
+notas.pop()     // pop = retira o ultimo item
+
+console.log(notas)
+
+let media = (notas[0]+notas[1])/ notas.length
+
+console.log(media)
