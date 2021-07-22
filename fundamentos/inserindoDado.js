67c2033915e03e7002eb4d94e062cf855f673702
Patrick <66400161+patrickfreiree@users.noreply.github.com>
2021-07-22 09:01:35 -0300

    Create inserindoDado.js

---
+++ fundamentos/inserindoDado.js
@@ -0,0 +1,8 @@
+const listaAlunos = ['ana','João','Maria','Patrick','Joana','Jose']
+listaAlunos.splice(1,2,'Onara')
+console.log(`Lista chamada alunos:  ${listaAlunos}`)
+
+// nao tiro ninguém
+const listaAlunos2 = ['ana','João','Maria','Patrick','Joana','Jose']
+listaAlunos2.splice(3,0,'OIOIO')
+console.log(`Lista chamada alunos2:  ${listaAlunos2}`)
